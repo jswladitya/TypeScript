97f49b31ff4aa902e8fc4eb64f5e94ffc8571007
jswladitya <jswladitya0403@gmail.com>
2024-11-19 20:14:28 +0530

    started learning typescript

---
+++ 1_Intro&Configuration/app.ts
@@ -0,0 +1,16 @@
+/*
+1. to compile ts code to js just write "tsc app.ts"
+
+2. "tsc --watch" -> for real time code compilaton to js
+
+3. "npx tsc --init" -> to make config file 
+
+*/
+
+var a = 1;
+var b = 2;
+
+const hello = function(){
+    console.log("Hello TypeScript");
+}
+
